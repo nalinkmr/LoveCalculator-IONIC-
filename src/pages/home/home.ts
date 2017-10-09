e14a753fc882a9ddc3550a64a4273be61fcceacf
nalin kumar <nalinkmrr@gmail.com>
2017-10-10 00:29:51 +0530

    uploaded

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+selector: 'page-home',
+templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage
+{
+name1='';
+name2='';
 
-  constructor(public navCtrl: NavController) {
-
+get score()
+{
+const letters=(this.name1 + this.name2).toLowerCase();
+let sum=0;
+for(let i=0;i<letters.length;i++)
+  {
+  sum=sum+letters.charCodeAt(i);
   }
-
-}
+  return sum % 101;
+  }
+  }
